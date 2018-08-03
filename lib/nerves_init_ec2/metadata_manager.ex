72c7d8182e33e4cbed24cf108ecccdf7e60ceac9
Jake Morrison <jake@cogini.com>
2018-08-03 22:51:25 +0000

    Wait for metadata to be available; Configure net_kernel

--- lib/nerves_init_ec2/metadata_manager.ex
+++ lib/nerves_init_ec2/metadata_manager.ex
@@ -1,5 +1,9 @@
 defmodule NervesInitEc2.MetadataManager do
-  @moduledoc "Configure SystemRegistry from EC2 metadata"
+  @moduledoc "Configure SystemRegistry from EC2 instance metadata"
+  # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html
+
+  # @ifname "eth0"
+  @metadata_sleep 200
 
   use GenServer
 
@@ -10,13 +14,31 @@ defmodule NervesInitEc2.MetadataManager do
   end
 
   def init([opts]) do
-    Logger.debug("#{__MODULE__}: opts: #{inspect opts}")
-    SystemRegistry.register()
+    # Logger.debug("#{__MODULE__} opts: #{inspect opts}")
+    # SystemRegistry.register()
 
     set_defaults(opts)
+    {:ok, count} = wait_for_metadata(0)
+    Logger.debug("Waited #{count * @metadata_sleep / 1_000} sec for metatdata")
 
-    {:ok, %{ip: nil, ifname: "eth0"}}
+    registry = SystemRegistry.match(:global, :_)
+    update_ssh_authorized_keys(registry)
+    start_net_kernel(opts)
+
+    {:ok, %{ip: nil}}
   end
+
+  # def handle_info({:system_registry, :global, registry}, state) do
+  #   new_ip = get_in(registry, [:state, :network_interface, @ifname, :ipv4_address])
+  #   if new_ip == state.ip do
+  #     {:noreply, state}
+  #   else
+  #     Logger.debug("IP address for #{@ifname} changed to #{new_ip}")
+  #     update_ssh_authorized_keys(registry)
+  #
+  #     {:noreply, %{state | ip: new_ip}}
+  #   end
+  # end
 
   def set_defaults(opts) do
     SystemRegistry.transaction(priority: :default)
@@ -25,40 +47,70 @@ defmodule NervesInitEc2.MetadataManager do
     |> SystemRegistry.commit
   end
 
-  def handle_info({:system_registry, :global, registry}, state) do
-    new_ip = get_in(registry, [:state, :network_interface, state.ifname, :ipv4_address])
-    if new_ip == state.ip do
-      {:noreply, state}
-    else
-      Logger.debug("IP address for #{state.ifname} changed to #{new_ip}")
-      update_ssh_authorized_keys(registry)
+  # @doc "Block waiting for metadata to be available"
+  defp wait_for_metadata(count) do
+    case :httpc.request('http://169.254.169.254/') do
+      {:ok, {{_http_version, 200, _reason_phrase}, _headers, ""}} ->
+        :timer.sleep(@metadata_sleep)
+        wait_for_metadata(count + 1)
+      {:ok, {{_http_version, 200, _reason_phrase}, _headers, _data}} ->
+        {:ok, count}
+      _ ->
+        :timer.sleep(@metadata_sleep)
+        wait_for_metadata(count + 1)
+    end
+  end
 
-      {:noreply, %{state | ip: new_ip}}
+  @spec get_metadata(String.t) :: {:ok, binary} | {:error, term}
+  def get_metadata(url) do
+    case :httpc.request(to_charlist(url)) do
+      {:ok, {{_http_version, 200, _reason_phrase}, _headers, data}} ->
+        {:ok, to_string(data)}
+      result ->
+        Logger.debug("Could not read instance metadata: #{inspect result}")
+        {:error, result}
     end
   end
 
   def update_ssh_authorized_keys(registry) do
-    Logger.info("Configuring from instance metadata")
+    authorized_keys = get_in(registry, [:config, :ssh, :authorized_keys]) || []
+    {:ok, instance_key} = get_metadata('http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key')
+    update_ssh_authorized_keys(instance_key, authorized_keys)
+  end
 
-    authorized_keys = get_in(registry, [:config, :ssh, :authorized_keys])
-
-    instance_keys = case :httpc.request('http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key') do
-      {:ok, {_, _, ""}} ->
-        []
-      {:ok, {_, _, key}} ->
-        [to_string(key)]
-      result ->
-        Logger.error("Error reading instance metadata: #{inspect result}")
-        []
-    end
-
-    all_keys = authorized_keys ++ instance_keys
-    Logger.debug("All ssh keys: #{inspect all_keys}")
-    if length(all_keys) > 0 do
+  defp update_ssh_authorized_keys(nil, _authorized_keys), do: :ok
+  defp update_ssh_authorized_keys(key, authorized_keys) do
+    if key not in authorized_keys do
+      Logger.info("Adding SSH key from instance metadata")
+      all_keys = authorized_keys ++ [key]
       SystemRegistry.update([:config, :ssh, :authorized_keys], all_keys)
       Application.put_env(:nerves_firmware_ssh, :authorized_keys, all_keys, persistent: true)
     end
+  end
 
+  defp start_net_kernel(%{net_kernel: true} = opts) do
+    node_name = node_name(opts.node_name, opts.node_name_public)
+    Logger.debug("Starting net_kernel with node name #{node_name}")
+
+    :os.cmd('epmd -daemon')
+    case :net_kernel.start([node_name]) do
+      {:ok, _} ->
+        Logger.debug("Started net_kernel as node #{node_name}")
+        :ok
+      {:error, reason} ->
+        Logger.error("Error starting net_kernel: #{reason}")
+        :error
+    end
+  end
+  defp start_net_kernel(_opts), do: :ok
+
+  defp node_name(node_name, true) do
+    {:ok, ip} = get_metadata("http://169.254.169.254/latest/meta-data/public-ipv4")
+    :"#{node_name}@#{ip}"
+  end
+  defp node_name(node_name, _) do
+    {:ok, ip} = get_metadata("http://169.254.169.254/latest/meta-data/local-ipv4")
+    :"#{node_name}@#{ip}"
   end
 
 end
